9a8186b8cf397cce336e5ba14740323182b4c555
hatake511522 <623dgk511522@gmail.com>
2020-02-21 00:43:10 +0900

    リファクタリング

--- reservation.py
+++ reservation.py
@@ -9,40 +9,44 @@ import os
 
 import settings
 
-USC = settings.USC
-USP = settings.USP
-WHU = settings.WHU
 
-webdriver = webdriver.Chrome()
+
 for i in range(1, 1000):
-  webdriver.get("https://yoyaku-f.koyama.co.jp/scripts/mtr1010.asp")
+  USC = settings.USC
+  USP = settings.USP
+  WHU = settings.WHU
 
-  webdriver.find_element_by_xpath(
+  driver = webdriver.Chrome()
+  driver.get("https://yoyaku-f.koyama.co.jp/scripts/mtr1010.asp")
+
+  driver.find_element_by_xpath(
     "/html/body/div/form/table/tbody/tr[1]/td[2]/input").send_keys(USC)
-  webdriver.find_element_by_xpath(
+  driver.find_element_by_xpath(
     "/html/body/div/form/table/tbody/tr[2]/td[2]/input").send_keys(USP)
-  webdriver.find_element_by_name("login").click()
+  driver.find_element_by_name("login").click()
   time.sleep(2)
-  webdriver.find_element_by_name("mtr1010").click()
+  driver.find_element_by_name("mtr1010").click()
   time.sleep(2)
 
-  kushya1 = webdriver.find_elements_by_xpath(
+  kushya1 = driver.find_elements_by_xpath(
     "//img[@src='ko2_kushya.gif']")
 
-  webdriver.find_element_by_xpath(
+  driver.find_element_by_xpath(
       "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input").click()
   time.sleep(1)
-  kushya2 = webdriver.find_elements_by_xpath(
+  kushya2 = driver.find_elements_by_xpath(
     "//img[@src='ko2_kushya.gif']")
 
-  webdriver.find_element_by_xpath(
+  driver.find_element_by_xpath(
       "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input").click()
   time.sleep(1)
-  kushya3 = webdriver.find_elements_by_xpath(
+  kushya3 = driver.find_elements_by_xpath(
       "//img[@src='ko2_kushya.gif']")
 
-  webdriver.find_element_by_xpath(
+  driver.find_element_by_xpath(
     "/html/body/div[1]/form[2]/input").click()
+  time.sleep(1)
+  driver.quit()
   
 
   empty_number = len(kushya1) + len(kushya2) + len(kushya3)
@@ -55,5 +59,5 @@ for i in range(1, 1000):
         "icon_url": "https://stickershop.line-scdn.net/stickershop/v1/product/1154602/LINEStorePC/main.png;compress=true",
         "text": text
     }))
-  time.sleep(120) #だいたい2分半間隔にしよう
+  time.sleep(20) #だいたい2分半間隔にしよう
 
