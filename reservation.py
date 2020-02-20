a23e939fd34e01e91eff6ccb304e200749f15f6d
hatake511522 <623dgk511522@gmail.com>
2020-02-20 23:31:52 +0900

    InitialCommit

---
+++ reservation.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import time
+import requests
+import json
+import os
+
+import settings
+
+USC = settings.USC
+USP = settings.USP
+WHU = settings.WHU
+
+webdriver = webdriver.Chrome()
+for i in range(1, 1000):
+  webdriver.get("https://yoyaku-f.koyama.co.jp/scripts/mtr1010.asp")
+
+  webdriver.find_element_by_xpath(
+    "/html/body/div/form/table/tbody/tr[1]/td[2]/input").send_keys(USC)
+  webdriver.find_element_by_xpath(
+    "/html/body/div/form/table/tbody/tr[2]/td[2]/input").send_keys(USP)
+  webdriver.find_element_by_name("login").click()
+  time.sleep(2)
+  webdriver.find_element_by_name("mtr1010").click()
+  time.sleep(2)
+
+  kushya1 = webdriver.find_elements_by_xpath(
+    "//img[@src='ko2_kushya.gif']")
+
+  webdriver.find_element_by_xpath(
+      "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input").click()
+  time.sleep(2)
+
+  kushya2 = webdriver.find_elements_by_xpath(
+    "//img[@src='ko2_kushya.gif']")
+
+  webdriver.find_element_by_xpath(
+      "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input").click()
+  time.sleep(2)
+
+  kushya3 = webdriver.find_elements_by_xpath(
+      "//img[@src='ko2_kushya.gif']")
+
+  empty_number = len(kushya1) + len(kushya2) + len(kushya3)
+  webhook_url = WHU
+  text = '現在コヤマに空きが%d件あります。急げ！' % empty_number
+
+  if empty_number > 0:
+    requests.post(webhook_url, data=json.dumps({
+        "text": text
+    }))
+  else:
+    requests.post(webhook_url, data=json.dumps({
+        "text": "現在空きはありません"
+    }))
+  time.sleep(120) #だいたい2分半間隔にしよう
+
---
+++ settings.py
@@ -0,0 +1,15 @@
+#実行すると .env ファイルに期日した認証情報を環境変数へ記述してくれる
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+USC = os.environ.get("USER_CODE")
+USP = os.environ.get("USER_PASSWORD")
+WHU = os.environ.get("WEBHOOK_URL")
+
+#NOTE: 直接 .env から変数を取得できないのか
