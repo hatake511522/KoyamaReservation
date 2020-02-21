e15d3b86e734efff7861415a70e3b3b9eb927253
hatake511522 <623dgk511522@gmail.com>
2020-02-21 09:59:25 +0900

    Fix #3, 例外が起きた際処理を初めから実行し直す

--- reservation.py
+++ reservation.py
@@ -1,90 +1,88 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 import time
 import requests
 import json
 import os
-
 import settings
 
 USC = settings.USC
 USP = settings.USP
 WHU = settings.WHU
 
-driver = webdriver.Chrome()
-driver.get("https://yoyaku-f.koyama.co.jp/scripts/mtr1010.asp")
+class reservationClass:
+  def main(self):
+    driver = webdriver.Chrome()
+    for i in range(1, 1000):
+      wait = WebDriverWait(driver, 15)
+      url = "https://yoyaku-f.koyama.co.jp/scripts/mtr1010.asp"
+      driver.get(url)
 
-driver.find_element_by_xpath(
-    "/html/body/div/form/table/tbody/tr[1]/td[2]/input").send_keys(USC)
-driver.find_element_by_xpath(
-    "/html/body/div/form/table/tbody/tr[2]/td[2]/input").send_keys(USP)
-driver.find_element_by_name("login").click()
-time.sleep(2)
-driver.find_element_by_name("mtr1010").click()
-time.sleep(2)
+      wait.until(EC.element_to_be_clickable((By.NAME, 'login')))
+      user_code_xpath = "/html/body/div/form/table/tbody/tr[1]/td[2]/input"
+      user_password_xpath = "/html/body/div/form/table/tbody/tr[2]/td[2]/input"
+      driver.find_element_by_xpath(user_code_xpath).send_keys(USC)
+      driver.find_element_by_xpath(user_password_xpath).send_keys(USP)
 
-kushya1 = driver.find_elements_by_xpath(
-    "//img[@src='ko2_kushya.gif']")
+      driver.find_element_by_name("login").click()  # ログインボタン
 
-driver.find_element_by_xpath(
-    "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input").click()
-time.sleep(1)
-kushya2 = driver.find_elements_by_xpath(
-    "//img[@src='ko2_kushya.gif']")
+      wait.until(EC.element_to_be_clickable((By.NAME, 'mtr1010')))
+      driver.find_element_by_name("mtr1010").click()  # 技能予約を選択
 
-driver.find_element_by_xpath(
-    "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input").click()
-time.sleep(1)
-kushya3 = driver.find_elements_by_xpath(
-    "//img[@src='ko2_kushya.gif']")
+      kushya1 = driver.find_elements_by_xpath(
+          "//img[@src='ko2_kushya.gif']")
 
-driver.find_element_by_xpath(
-    "/html/body/div[1]/form[2]/input").click()
-time.sleep(1)
+      next_display_path1 = "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input"
+      wait.until(EC.element_to_be_clickable((By.XPATH, next_display_path1)))
+      driver.find_element_by_xpath(next_display_path1).click()  # 次画面に遷移
+
+      time.sleep(5)
+      kushya2 = driver.find_elements_by_xpath(
+          "//img[@src='ko2_kushya.gif']")
+
+      next_display_path2 = "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input"
+      wait.until(EC.element_to_be_clickable((By.XPATH, next_display_path2)))
+      driver.find_element_by_xpath(next_display_path2).click()  # 次画面に遷移
+
+      time.sleep(5)
+      kushya3 = driver.find_elements_by_xpath(
+          "//img[@src='ko2_kushya.gif']")
+
+      logout_path = "/html/body/div[1]/form[2]/input"
+      wait.until(EC.element_to_be_clickable((By.XPATH, logout_path)))
+      driver.find_element_by_xpath(logout_path).click()
+      time.sleep(5)
+
+      empty_number = len(kushya1) + len(kushya2) + len(kushya3)
+      webhook_url = WHU
+      text = '現在コヤマに空きが%d件あります。急げ！' % empty_number
+      if empty_number > 0:
+        requests.post(webhook_url, data=json.dumps({
+            "username": "PAPARU君",
+            "icon_url": "https://stickershop.line-scdn.net/stickershop/v1/product/1154602/LINEStorePC/main.png;compress=true",
+            "text": text
+        }))
+      time.sleep(180)
+    pass
+  def back_to_home(self):
+    driver = webdriver.Chrome()
+    url = "https://yoyaku-f.koyama.co.jp/scripts/mtr1010.asp"
+    driver.get(url)
+    pass
 
 
-for i in range(1, 1000):
-  driver.find_element_by_xpath(
-    "/html/body/div/form/table/tbody/tr[1]/td[2]/input").send_keys(USC)
-  driver.find_element_by_xpath(
-    "/html/body/div/form/table/tbody/tr[2]/td[2]/input").send_keys(USP)
-  driver.find_element_by_name("login").click() #ログインボタン
-  time.sleep(2)
+if __name__ == '__main__':
+    while True:
+        try:
+            reservationClass().main()
+          
 
-  driver.find_element_by_name("mtr1010").click() #技能予約を選択
-  time.sleep(2)
-
-  kushya1 = driver.find_elements_by_xpath(
-    "//img[@src='ko2_kushya.gif']")
-
-  driver.find_element_by_xpath(
-      "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input").click() #次画面に遷移
-  time.sleep(1)
-  kushya2 = driver.find_elements_by_xpath(
-    "//img[@src='ko2_kushya.gif']")
-
-  driver.find_element_by_xpath(
-      "/html/body/div[1]/form[1]/table[1]/tbody/tr[3]/td[3]/input").click()
-  time.sleep(1)
-  kushya3 = driver.find_elements_by_xpath(
-      "//img[@src='ko2_kushya.gif']")
-
-  driver.find_element_by_xpath(
-    "/html/body/div[1]/form[2]/input").click()
-  time.sleep(1)
-  
-
-  empty_number = len(kushya1) + len(kushya2) + len(kushya3)
-  webhook_url = WHU
-  text = '現在コヤマに空きが%d件あります。急げ！' % empty_number
-
-  if empty_number > 0:
-    requests.post(webhook_url, data=json.dumps({
-        "username": "PAPARU君",
-        "icon_url": "https://stickershop.line-scdn.net/stickershop/v1/product/1154602/LINEStorePC/main.png;compress=true",
-        "text": text
-    }))
-  time.sleep(120) #だいたい2分半間隔にしよう
+        except TimeoutException as te: 
+            reservationClass().back_to_home()
+            reservationClass().main()
 
